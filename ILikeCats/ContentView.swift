795120380d2291b32567566183c185661a58fba1
kwe <kwevans@gmail.com>
2022-03-18 14:54:28 +0000

    Initial Commit

---
+++ ILikeCats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ILikeCats
+//
+//  Created by Kevin W Evans on 18/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ILikeCats/ILikeCatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ILikeCatsApp.swift
+//  ILikeCats
+//
+//  Created by Kevin W Evans on 18/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ILikeCatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
